a9b2c8c85183a4a1b2e32a5d9c77cc3e973a8baf
terippd <thasarit@ippd.or.th>
2020-02-12 16:52:47 +0700

    useauth hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React from 'react';
+import { withRouter, NavLink } from 'react-router-dom';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="flex">
+        <img className="logo" src="/logo.png" alt="Hooks News Logo" />
+        <NavLink className="header-title" to="/">
+          Hooks News
+        </NavLink>
+        <NavLink className="header-link" to="/">
+          new
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink className="header-link" to="/top">
+          top
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink className="header-link" to="/search">
+          search
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink className="header-link" to="/create">
+          submit
+        </NavLink>
+      </div>
+      <div className="flex">
+        <NavLink className="header-link" to="/login">
+          login
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
-export default Header;
+export default withRouter(Header);
